1589cb0c942cc34eb425f44f736b8c87d8e18d36
lounguyen68 <lou9.works@gmail.com>
2024-11-13 21:47:25 +0700

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+  ],
+})
+export class AppModule {}
